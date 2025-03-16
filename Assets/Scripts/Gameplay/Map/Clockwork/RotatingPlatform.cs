782c145f72712b55e673f7a4572c1f656316d3a0
Gabe Kotton <52292616+FlamingoeZpZ@users.noreply.github.com>
2025-03-16 17:04:32 -0400

    ..

--- Assets/Scripts/Gameplay/Map/Clockwork/RotatingPlatform.cs
+++ Assets/Scripts/Gameplay/Map/Clockwork/RotatingPlatform.cs
@@ -8,13 +8,11 @@ namespace Gameplay.Map.Clockwork
     [RequireComponent(typeof(NetworkObject), typeof(NetworkTransform))]
     public class RotatingPlatform : MonoBehaviour
     {
-
         [SerializeField] private Vector3 rotationAxis = new Vector3(0,1,0);
         [SerializeField, Tooltip("How far a \'tick\' really is")] private float tickDegrees = 2;
         [SerializeField, Tooltip("Time between rotations, can be used to make it step")] private float timeBetweenRotations; // Use this to make it look like it's "Ticking"
 
         private float _currentTime;
-        private float _currentRotation;
 
         private void Awake()
         {
@@ -28,9 +26,7 @@ namespace Gameplay.Map.Clockwork
 
             if (_currentTime < timeBetweenRotations) return;
             _currentTime = 0;
-            _currentRotation = (tickDegrees + _currentRotation) % 360f;
-            
-            transform.localRotation = Quaternion.AngleAxis(_currentRotation, rotationAxis);
+            transform.localRotation *= Quaternion.AngleAxis(tickDegrees, rotationAxis);
         }
     }
 }
