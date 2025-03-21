d3a72439f102118bc15d7eb85fd459156252f18a
Carlos_Smoonthy_Lu <74547522+CarlosEOL@users.noreply.github.com>
2025-03-20 21:28:13 -0400

    asd
    ASDASDASDsa

--- Assets/Scripts/Managers/Local/FMODEvents.cs
+++ Assets/Scripts/Managers/Local/FMODEvents.cs
@@ -30,6 +30,10 @@ namespace Managers.Local
         [field: Header("Customization Click")]
         [field: SerializeField]
         public EventReference click { get; private set; }
+        
+        [field: Header("Customization Click")]
+        [field: SerializeField]
+        public List<EventReference> Beep { get; private set; }
 
         public static FMODEvents instance { get; private set; }
 
