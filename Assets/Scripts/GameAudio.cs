35bb9ce950857c0f728b800fc81a69f28ae4add8
Carlos_Smoonthy_Lu <74547522+CarlosEOL@users.noreply.github.com>
2025-03-31 00:13:13 -0400

    This.

--- Assets/Scripts/GameAudio.cs
+++ Assets/Scripts/GameAudio.cs
@@ -1,13 +1,13 @@
-using System;
+using Managers.Local;
 using Managers.Network;
 using UnityEngine;
 
 /// <summary>
 /// THIS IS FOR THE GAME AUDIO -- CAN BE FOUND IN THE MANAGERS PREFAB ON THE NETWORKGAMEMANAGER, ALL OF THIS IS REPLICATED SO EVERYBODY WILL PLAY THEIR OWN COPY OF THE AUDIO
 /// </summary>
+///
 public class GameAudio : MonoBehaviour
 {
-
     private void OnEnable()
     {
         NetworkGameManager.Instance.OnGameReachedOverTime += OnGameReachedOverTime;
@@ -24,15 +24,24 @@ public class GameAudio : MonoBehaviour
     /// <summary>
     /// CALLED WHEN THE GAME HAS REACHED OVERTIME, EVERY CLIENT WILL PLAY THIS AUDIO SO JUST DO IT NORMALLY
     /// </summary>
-    private void OnGameReachedOverTime() { }
-
-
+    private void OnGameReachedOverTime()
+    {
+        AudioManager.instance.TriggerGameState(1);
+    }
+    
     /// <summary>
     /// IF THIS VALUE IS 0, IT MEANS THE TEXT IS SAYING "GO", OTHERWISE IT WILL BE 3,2,1... GO!
     /// </summary>
     /// <param name="value"></param>
     private void OnGameStartCountdownDecremented(int value)
     {
-        Debug.Log("GAME COUNTDOWN DECREMENTED!");
+        if (value == 0)
+        {
+            AudioManager.instance.PlayOneShot(FMODEvents.instance.Beep[1], transform.position);
+        }
+        else if (value > 0 && value < 4)
+        {
+            AudioManager.instance.PlayOneShot(FMODEvents.instance.Beep[0], transform.position);
+        }
     }
 }
