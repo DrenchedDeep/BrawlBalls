0abed5b6c2c57db55461653bfa0aec6ea8e7b840
Gabe Kotton <52292616+FlamingoeZpZ@users.noreply.github.com>
2025-03-31 02:45:37 -0400

    Fixed Settings

---
+++ Assets/Scripts/Loading/LoadingCheckpoints/UrgentSettingsCheckpoint.cs
@@ -0,0 +1,37 @@
+using System;
+using Cysharp.Threading.Tasks;
+using FMODUnity;
+using UnityEngine;
+
+namespace Loading.LoadingCheckpoints
+{
+    public class UrgentSettingsCheckpoint : MonoBehaviour, ILoadingCheckpoint
+    {
+
+        public Action OnComplete { get; set; }
+        public Action OnFailed { get; set; }
+
+        private static bool _isComplete;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        private static void RuntimeInit()
+        {
+            _isComplete = false;
+        }
+
+        public UniTask Execute()
+        {
+            RuntimeManager.GetBus("Bus:/").setVolume(PlayerPrefs.GetFloat("Audio_Bus:/", 0.5f));
+            RuntimeManager.GetBus("Bus:/Music").setVolume(PlayerPrefs.GetFloat("Audio_Bus:/Music", 0.5f));
+            RuntimeManager.GetBus("Bus:/SFXs").setVolume(PlayerPrefs.GetFloat("Audio_Bus:/SFXs", 0.5f));
+            RuntimeManager.GetBus("Bus:/Ambience").setVolume(PlayerPrefs.GetFloat("Audio_Bus:/Ambience", 0.5f));
+            _isComplete = true;
+            return UniTask.CompletedTask;
+        }
+
+        public bool IsCompleted()
+        {
+            return _isComplete;
+        }
+    }
+}
