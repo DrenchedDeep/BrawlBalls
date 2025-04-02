7c77f530c7768c721a31bae3f418b54a034d9a3d
SebF4 <sebastian.finkbeiner@ontariotechu.net>
2025-04-02 13:51:05 -0400

    farting my whole ass off

--- Assets/Scripts/Gameplay/Object Scripts/Collectable.cs
+++ Assets/Scripts/Gameplay/Object Scripts/Collectable.cs
@@ -1,5 +1,6 @@
 using Managers;
 using Managers.Local;
+using System.Diagnostics;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -7,17 +8,25 @@ namespace Gameplay.Object_Scripts
 {
     public class Collectable : NetworkBehaviour
     {
-    
-
         [SerializeField] private ParticleManager.ECollectableType collectableType;
-    
-    
+        // Add a reference to your particle system prefab (set it up in the Inspector)
+        [SerializeField] private ParticleSystem particleEffectPrefab;
+
         protected void Award(BallPlayer getComponent)
         {
-            //Award this collectable type to the owner.
+            // Award this collectable type to the owner.
             getComponent.GiveAward(collectableType);
-            //Play particle...
-            
+
+            // Spawn the particle effect at the collectable's position
+            ParticleSystem ps = Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
+            ps.Play();
+
+            // Get the main module to access the duration
+            ParticleSystem.MainModule mainModule = ps.main;
+            // Destroy the particle system gameObject after its duration
+            Destroy(ps.gameObject, mainModule.duration);
+
+            // Disable the collectable object (networked)
             ToggleState_ServerRpc(false);
         }
 
@@ -26,17 +35,11 @@ namespace Gameplay.Object_Scripts
         {
             gameObject.SetActive(state);
         }
-        
-        
-    
 
         protected virtual void OnTriggerEnter(Collider other)
         {
-            Debug.LogWarning("Object collected");
+            //Debug.LogWarning("Object collected");
             Award(other.transform.parent.GetComponent<BallPlayer>());
         }
-    
-    
-    
     }
 }
